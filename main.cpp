0b46c2fec6ec2abc71550f4ddb176a51b9e3d2a4
guilhermecpp <guilhermecdemenezes123@gmail.com>
2020-05-19 08:20:57 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int main()
 {
+    int qtd_compras;
+    double ticket_medio;
+
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+    cout << endl;
+
+    cout << "Quantas compras o cliente fez no ultimo ano? ";
+    cin >> qtd_compras;
+    cout << "Qual o ticket medio? ";
+    cin >> ticket_medio;
     cout << endl;
 
     return 0;
