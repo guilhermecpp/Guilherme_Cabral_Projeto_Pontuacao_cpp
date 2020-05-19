95ea6521e5d66d254ca72501fd6c968e6df6f723
guilhermecpp <guilhermecdemenezes123@gmail.com>
2020-05-19 08:19:23 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+    cout << endl;
+
     return 0;
 }
