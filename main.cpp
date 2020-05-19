13be7cc416a9484345911023247148aa04743fe0
guilhermecpp <guilhermecdemenezes123@gmail.com>
2020-05-19 08:22:04 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main()
     int qtd_compras;
     double ticket_medio;
 
+    int atrasos;
+    char forma_pagamento;
+
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
@@ -15,6 +18,12 @@ int main()
     cin >> qtd_compras;
     cout << "Qual o ticket medio? ";
     cin >> ticket_medio;
+    cout << endl;
+
+    cout << "Quantas vezes o cliente atrasou o pagamento? ";
+    cin >> atrasos;
+    cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)? ";
+    cin >> forma_pagamento;
     cout << endl;
 
     return 0;
