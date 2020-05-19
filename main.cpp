dd2995fd30eb588785ed8d62bcb8efe7f78e56eb
guilhermecpp <guilhermecdemenezes123@gmail.com>
2020-05-19 08:23:20 -0300

    Score de volume de compras

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ int main()
     int atrasos;
     char forma_pagamento;
 
+    int score_volume_de_compras;
+    double volume_de_compras;
+
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
@@ -24,6 +27,31 @@ int main()
     cin >> atrasos;
     cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)? ";
     cin >> forma_pagamento;
+    cout << endl;
+
+    volume_de_compras = qtd_compras * ticket_medio;
+
+    if(qtd_compras == 0)
+    {
+        score_volume_de_compras = 0;
+    }else
+    {
+        if(volume_de_compras <= 3000.0)
+        {
+            if(qtd_compras <= 2)
+            {
+                score_volume_de_compras = 20;
+            }else
+            {
+                score_volume_de_compras = 40;
+            }
+        }else
+        {
+            score_volume_de_compras = 60;
+        }
+    }
+
+    cout << "Score de volume de compras = " << score_volume_de_compras << " pontos" << endl;
     cout << endl;
 
     return 0;
